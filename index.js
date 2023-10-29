5cd169b0b7bafc692ea0af2a35d0aed10f0ff60d
omparkash4023 <omparkash.op4023@gmail.com>
2023-10-29 06:46:43 +0530

    backend flow api created 281023

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ db.connect((error) => {
   console.log("Connected to MySQL database");
 });
 
-// singup api
+// ********************************** signup api starts ********************************
 app.post("/post/signup", async (req, res) => {
   const { username, email, password } = req.body;
 
@@ -77,15 +77,16 @@ app.post("/post/signup", async (req, res) => {
             console.error("Table error:", err);
             return res.status(500).json({ message: "Internal server error" });
           }
-          console.log("Data inserted");
+          // console.log("Data inserted");
           res.status(200).json({ message: "Form submitted successfully" });
         }
       );
     }
   });
 });
+// ********************************** signup api ends ********************************
 
-// login api
+// ********************************** login api starts ********************************
 app.post("/post/login", async (req, res) => {
   // res.status(200).json({ message: "form submitted successfully" });
   const { email, password } = req.body;
@@ -109,7 +110,7 @@ app.post("/post/login", async (req, res) => {
       const validPassword = await bcrypt.compare(password, results[0].password);
 
       if (validPassword) {
-        console.log("Login successfully");
+        // console.log("Login successfully");
         // const token = jwt.sign({ results, validPassword }, secretKey, {expiresIn: "1h",});
         const token = jwt.sign({ results, validPassword }, secretKey);
         return res
@@ -121,8 +122,9 @@ app.post("/post/login", async (req, res) => {
     }
   });
 });
+// ********************************** login api ends ********************************
 
-// Middleware to verify JWT token
+// ********************************** Middleware to verify JWT token starts ********************************
 const verifyToken = (req, res, next) => {
   const token = req.headers.authorization;
   if (!token) {
@@ -139,8 +141,9 @@ const verifyToken = (req, res, next) => {
       .json({ message: "Token verification failed", error: error.message });
   }
 };
+// ********************************** Middleware to verify JWT token ends ********************************
 
-// protected api
+// ********************************** protected routes api starts ********************************
 app.get("/protected/routes", verifyToken, async (req, res) => {
   const token = req.headers.authorization;
 
@@ -149,8 +152,328 @@ app.get("/protected/routes", verifyToken, async (req, res) => {
   }
 
   res.status(200).json({ message: "success", user: req.user });
-  console.log("req.userrrrr", req.user);
+  // console.log("req.userrrrr", req.user);
 });
+// ********************************** protected routes api ends ********************************
+
+// ********************************** cartProducts post api starts ********************************
+app.post("/post/cartproducts", (req, res) => {
+  const {
+    id,
+    title,
+    price,
+    quantity,
+    brand,
+    category,
+    description,
+    discountPercentage,
+    stock,
+    thumbnail,
+    rating,
+    email,
+  } = req.body;
+  // console.log("req.bodyyyyy", req.body);
+
+  // Check if a record with the same email and title exists
+  const checkIfExistsQuery =
+    "SELECT id FROM cartproducts WHERE id = ? AND email = ?";
+  db.query(checkIfExistsQuery, [id, email], (error, existingProduct) => {
+    if (error) {
+      console.error("Error checking if product exists", error);
+      return res.status(500).json({ message: "Internal server error" });
+    } else {
+      if (existingProduct.length > 0) {
+        // A product with the same email and title already exists, update the record
+        const updateProductQuery =
+          "UPDATE cartproducts SET title = ?, price = ?, quantity = ?, brand = ?, category = ?, description = ?, discountPercentage = ?, stock = ?, thumbnail = ?, rating = ? WHERE id = ? AND email = ?";
+        db.query(
+          updateProductQuery,
+          [
+            title,
+            price,
+            quantity,
+            brand,
+            category,
+            description,
+            discountPercentage,
+            stock,
+            thumbnail,
+            rating,
+            id,
+            email,
+          ],
+          (updateError, updateResult) => {
+            if (updateError) {
+              console.error("Error updating product", updateError);
+              return res.status(500).json({ message: "Internal server error" });
+            }
+            // console.log("Product updated");
+            res.status(200).json({
+              message: "CartProduct updated successfully",
+              data: updateResult,
+            });
+          }
+        );
+      } else {
+        // No existing product found, insert a new one
+        const insertCartQuery =
+          "INSERT INTO cartproducts (id, title, price, quantity, brand, category, description, discountPercentage, stock, thumbnail, rating, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        db.query(
+          insertCartQuery,
+          [
+            id,
+            title,
+            price,
+            quantity,
+            brand,
+            category,
+            description,
+            discountPercentage,
+            stock,
+            thumbnail,
+            rating,
+            email,
+          ],
+          (insertError, insertResult) => {
+            if (insertError) {
+              console.error("cartProduct insert error", insertError);
+              return res.status(500).json({ message: "Internal server error" });
+            }
+            // console.log("CartProduct inserted");
+            res.status(200).json({
+              message: "CartProduct added successfully",
+              data: insertResult,
+            });
+          }
+        );
+      }
+    }
+  });
+});
+// ********************************** cartProducts post api ends ********************************
+
+
+// ********************************** get cart products api starts ********************************
+app.post("/get/cartproducts", (req, res) => {
+  const { email } = req.body;
+  // console.log("req.body", email);
+  // res.send("success")
+  const checkEmailMatch = "SELECT * FROM cartproducts WHERE email = ?";
+  db.query(checkEmailMatch, [email], (error, results) => {
+    if (error) {
+      console.error("Error fetching cart products", error);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+
+    // console.log("Cart products retrieved");
+    res
+      .status(200)
+      .json({ message: "Cart products retrieved successfully", data: results });
+  });
+});
+// ********************************** get cart products api ends ********************************
+
+
+// ********************************** update products quantity api starts ********************************
+app.put("/update/cartproducts/quantity", (req, res) => {
+  const { id, quantity, email } = req.body;
+
+  // Update the database for this product
+  const updateCartQuery =
+    "UPDATE cartproducts SET quantity = ? WHERE id = ? AND email = ?";
+
+  db.query(updateCartQuery, [quantity, id, email], (error, result) => {
+    if (error) {
+      console.error("cartProduct update error", error);
+    } else {
+      console.log(`CartProduct with ID ${id} updated successfully`);
+    }
+  });
+
+  // Send a response back to the frontend
+  res.status(200).json({ message: "CartProducts updated successfully" });
+});
+// ********************************** update products quantity api ends ********************************
+
+
+// ********************************** cartProducts delete api starts ********************************
+app.delete("/delete/cartproducts", (req, res) => {
+  const { id, email } = req.query;
+  // console.log("id, email", id, email);
+  // res.send("success");
+
+  const deleteQuert = `DELETE FROM cartproducts WHERE id = ? AND email = ?`;
+  db.query(deleteQuert, [id, email], (error, result) => {
+    if (error) {
+      console.error("Error product not exists", error);
+      return res.status(500).json({ message: "Internal server error" });
+    } else {
+      res.status(200).json({ message: "Record deleted successfully" });
+    }
+  });
+});
+// ********************************** cartProducts delete api ends ********************************
+
+
+// ********************************** wishlistProducts post api starts ********************************
+app.post("/post/wishlistproducts", (req, res) => {
+  const {
+    id,
+    title,
+    price,
+    quantity,
+    brand,
+    category,
+    description,
+    discountPercentage,
+    stock,
+    thumbnail,
+    rating,
+    email,
+  } = req.body;
+  // console.log("post/wishlistproducts bodyyyyy", req.body);
+
+  // Check if a record with the same email and title exists
+  const checkIfExistsQuery =
+    "SELECT id FROM wishlistproducts WHERE id = ? AND email = ?";
+  db.query(checkIfExistsQuery, [id, email], (error, existingProduct) => {
+    if (error) {
+      console.error("Error checking if product exists", error);
+      return res.status(500).json({ message: "Internal server error" });
+    } else {
+      if (existingProduct.length > 0) {
+        // A product with the same email and title already exists, update the record
+        const updateProductQuery =
+          "UPDATE wishlistproducts SET title = ?, price = ?, quantity = ?, brand = ?, category = ?, description = ?, discountPercentage = ?, stock = ?, thumbnail = ?, rating = ? WHERE id = ? AND email = ?";
+        db.query(
+          updateProductQuery,
+          [
+            title,
+            price,
+            quantity,
+            brand,
+            category,
+            description,
+            discountPercentage,
+            stock,
+            thumbnail,
+            rating,
+            id,
+            email,
+          ],
+          (updateError, updateResult) => {
+            if (updateError) {
+              console.error("Error updating product", updateError);
+              return res.status(500).json({ message: "Internal server error" });
+            }
+            // console.log("Product updated");
+            res.status(200).json({
+              message: "CartProduct updated successfully",
+              data: updateResult,
+            });
+          }
+        );
+      } else {
+        // No existing product found, insert a new one
+        const insertCartQuery =
+          "INSERT INTO wishlistproducts (id, title, price, quantity, brand, category, description, discountPercentage, stock, thumbnail, rating, email) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        db.query(
+          insertCartQuery,
+          [
+            id,
+            title,
+            price,
+            quantity,
+            brand,
+            category,
+            description,
+            discountPercentage,
+            stock,
+            thumbnail,
+            rating,
+            email,
+          ],
+          (insertError, insertResult) => {
+            if (insertError) {
+              console.error("cartProduct insert error", insertError);
+              return res.status(500).json({ message: "Internal server error" });
+            }
+            // console.log("CartProduct inserted");
+            res.status(200).json({
+              message: "CartProduct added successfully",
+              data: insertResult,
+            });
+          }
+        );
+      }
+    }
+  });
+});
+// ********************************** wishlistProducts post api ends ********************************
+
+
+// ********************************** get wishlistProducts api starts ********************************
+app.post("/get/wishlistproducts", (req, res) => {
+  const { email } = req.body;
+  // console.log("req.body", email);
+  // res.send("success")
+  const checkEmailMatch = "SELECT * FROM wishlistproducts WHERE email = ?";
+  db.query(checkEmailMatch, [email], (error, results) => {
+    if (error) {
+      console.error("Error fetching cart products", error);
+      return res.status(500).json({ message: "Internal server error" });
+    }
+
+    // console.log("Cart products retrieved");
+    res
+      .status(200)
+      .json({ message: "Cart products retrieved successfully", data: results });
+  });
+});
+// ********************************** get cwishlistProducts api ends ********************************
+
+
+// ********************************** update products quantity api starts ********************************
+app.put("/update/wishlistproducts/quantity", (req, res) => {
+  const { id, quantity, email } = req.body;
+  // console.log("/update/wishlistproducts/quantity", id, quantity, email);
+
+  // Update the database for this product
+  const updateCartQuery =
+    "UPDATE wishlistproducts SET quantity = ? WHERE id = ? AND email = ?";
+
+  db.query(updateCartQuery, [quantity, id, email], (error, result) => {
+    if (error) {
+      console.error("wishlistProduct update error", error);
+    } else {
+      console.log(`wishlistProduct with ID ${id} updated successfully`);
+    }
+  });
+
+  // Send a response back to the frontend
+  res.status(200).json({ message: "wishlistproducts updated successfully" });
+});
+// ********************************** update products quantity api ends ********************************
+
+
+// ********************************** wishlistproducts delete api starts ********************************
+app.delete("/delete/wishlistproducts", (req, res) => {
+  const { id, email } = req.query;
+  // console.log("id, email", id, email);
+  // res.send("success");
+
+  const deleteQuert = `DELETE FROM wishlistproducts WHERE id = ? AND email = ?`;
+  db.query(deleteQuert, [id, email], (error, result) => {
+    if (error) {
+      console.error("Error product not exists", error);
+      return res.status(500).json({ message: "Internal server error" });
+    } else {
+      res.status(200).json({ message: "Record deleted successfully" });
+    }
+  });
+});
+// ********************************** wishlistproducts delete api ends ********************************
+
 
 // Listen port
 app.listen(port, () => {
